0ebc52741bd4ed4d5178bacef652018f15a4fae8
RegistryROK <registry.fordev@gmail.com>
2023-10-21 16:07:24 +0900

    fix: delete src_d prop

--- components/Layouts/Buttons/OutLinedImageButton/index.tsx
+++ components/Layouts/Buttons/OutLinedImageButton/index.tsx
@@ -1,26 +1,22 @@
 'use clinet';
 
+import OutLinedButton from '@/components/Layouts/Buttons/OutLinedButton';
 import { ButtonProps } from '@mui/material';
 import Image from 'next/image';
-import OutLinedButton from '@/components/Layouts/Buttons/OutLinedButton';
 
 interface Props extends ButtonProps {
   image: {
     src: string;
-    src_d: string;
     alt: string;
   };
 }
 
 export default function OutLinedImageButton(props: Props) {
-  const { image, disabled, ...restProps } = props;
-  const imageSrc = !disabled ? image.src : image.src_d;
+  const { image, ...restProps } = props;
   return (
-    <OutLinedButton
-      disabled={disabled}
-      {...restProps}>
+    <OutLinedButton {...restProps}>
       <Image
-        src={imageSrc}
+        src={image.src}
         alt={image.alt}
         width={24}
         height={24}
